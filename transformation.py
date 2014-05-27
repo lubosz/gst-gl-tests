871304880c0c637706522c481480af4b85ab5907
Lubosz Sarnecki <lubosz@gmail.com>
2014-05-27 20:19:46 +0200

    manipulate gltransformation widget

--- transformation.py
+++ transformation.py
@@ -1,18 +1,51 @@
 #!/usr/bin/env python3
 
-from gi.repository import Gdk
-from gi.repository import Gst
-from gi.repository import Gtk
-from gi.repository import GdkX11    # for window.get_xid()
-from gi.repository import GstVideo  # for sink.set_window_handle()
+from IPython import embed
+
+from gi.repository import Gdk, Gst, Gtk, GdkX11, GstVideo
 
 import signal
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
-def window_closed (widget, event, pipeline):
-  widget.hide()
+def property_cb(scale, element, property):
+  element.set_property(property, scale.get_value())
+
+def quit(window):
+  window.hide()
   pipeline.set_state(Gst.State.NULL)
   Gtk.main_quit ()
+
+def window_closed (widget, event, pipeline):
+  quit(widget)
+
+def key_pressed (widget, key):
+  if key.keyval == Gdk.KEY_Escape:
+    quit(widget)
+
+class ElementScale(Gtk.Box):
+  def __init__(self, element, property, min, max, step, init):
+    Gtk.Box.__init__(self)
+
+    label = Gtk.Label(property)
+    scale = Gtk.Scale.new_with_range(Gtk.Orientation.HORIZONTAL, min, max, step)
+    
+    scale.set_property("expand", True)
+    scale.set_value(init)
+    scale.connect("value-changed", property_cb, element, property)
+    
+    self.set_orientation(Gtk.Orientation.HORIZONTAL)
+    
+    self.add(label)
+    self.add(scale)
+
+class Video(Gtk.DrawingArea): 
+  def __init__(self, w, h):
+    Gtk.DrawingArea.__init__(self)
+    self.set_size_request (w, h)
+    self.set_double_buffered (True)
+    
+  def xid(self):
+    return self.get_window().get_xid()
 
 if __name__=="__main__":
   Gdk.init([])
@@ -22,6 +55,7 @@ if __name__=="__main__":
   pipeline = Gst.Pipeline()
   src = Gst.ElementFactory.make("gltestsrc", None)
   transform = Gst.ElementFactory.make("gltransformation", None)
+  #sink = Gst.ElementFactory.make("fpsdisplaysink", None)
   sink = Gst.ElementFactory.make("glimagesink", None)
 
   if not sink or not src:
@@ -34,18 +68,35 @@ if __name__=="__main__":
 
   window = Gtk.Window()
   window.connect("delete-event", window_closed, pipeline)
-  window.set_default_size (1280, 720)
-  window.set_title ("Hello OpenGL Sink!")
+  window.connect("key-press-event", key_pressed)
+  #window.set_default_size (1280, 720)
+  window.set_title ("OpenGL Transformation")
 
-  drawing_area = Gtk.DrawingArea()
-  drawing_area.set_double_buffered (True)
-  window.add (drawing_area)
+  box = Gtk.Box()
+  box.set_orientation(Gtk.Orientation.VERTICAL)
+  
+  video = Video(1280, 720)
+  box.add(video)
+
+  #box.add(LabelScale("Rotation", 0, 360, 1, 0, red_cb, sink))
+  #box.add(LabelScale("X", -100, 100, 1, 0, red_cb, sink))
+  #box.add(LabelScale("Y", -100, 100, 1, 0, red_cb, sink))
+  #box.add(LabelScale("Scale", 0, 50, 1, 1, red_cb, sink))
+  
+  box.add(ElementScale(transform, "red", 0, 1, 0.005, 0))
+  box.add(ElementScale(transform, "green", 0, 1, 0.005, 0))
+  box.add(ElementScale(transform, "blue", 0, 1, 0.005, 0))
+  box.add(ElementScale(transform, "fovy", 0, 180, 0.5, 45))
+  box.add(ElementScale(transform, "aspect", 0, 100, 0.5, 0))
+  box.add(ElementScale(transform, "znear", 0, 100, 0.1, 0.1))
+  box.add(ElementScale(transform, "zfar", 0, 1000, 5, 100))
+  
+  window.add(box)
 
   window.show_all()
   window.realize()
   
-  xid = drawing_area.get_window().get_xid()
-  sink.set_window_handle (xid)
+  sink.set_window_handle(video.xid())
   
   if pipeline.set_state(Gst.State.PLAYING) == Gst.StateChangeReturn.FAILURE:
     pipeline.set_state(Gst.State.NULL)
